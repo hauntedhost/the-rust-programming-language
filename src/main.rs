6fea00424b49bf67f3c6c4ea78a4aaa894405670
Sean Omlor <somlor@eml.cc>
2018-11-10 23:57:15 -0500

    read and echo guess input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    print!("Please input your guess: ");
+    io::stdout().flush().expect("Failed to flush");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
