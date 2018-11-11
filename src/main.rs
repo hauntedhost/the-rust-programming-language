241be1d992fed355e0d5d58a50d0c363ba5b38fa
Sean Omlor <somlor@eml.cc>
2018-11-11 00:28:43 -0500

    generate secret and show result

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering::{Equal, Greater, Less};
 use std::io;
 use std::io::Write;
 
 fn main() {
+    let secret = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret);
+
     println!("Guess the number!");
 
     print!("Please input your guess: ");
@@ -12,5 +19,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Not a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Less => println!("Too small!"),
+        Greater => println!("Too big!"),
+        Equal => println!("Hell yeah."),
+    }
 }
